e7cdd5e0098fc874069c3620b2c9112d85423a0b
Jhony Freitas <jhony00._@hotmail.com>
2024-09-20 11:26:14 -0300

    Adicionando scripts

---
+++ scripts/delete-images-by-path.js
@@ -0,0 +1,47 @@
+// Esse script remove imagens do diretório que não estiver no arquivo .txt (imagens não excluídas do banco)
+
+const fs = require("fs");
+const path = require("path");
+
+// Caminho do diretório com as imagens
+const dirImages = "caminho-das-imagens";
+
+// Caminho para o arquivo com a lista de imagens que deseja manter
+const pathImageTxtFile = "caminho-do-arquivo.txt";
+
+const currentImages = new Set(
+  fs
+    .readFileSync(pathImageTxtFile, "utf-8")
+    .split("\n")
+    .map((img) => img.trim())
+);
+
+fs.readdir(dirImages, (err, arquivos) => {
+  if (err) {
+    console.error("Erro ao ler o diretório: ", err);
+    return;
+  }
+
+  arquivos.forEach((arquivo) => {
+    if (!currentImages.has(arquivo)) {
+      const imagePath = path.join(dirImages, arquivo);
+
+      fs.stat(imagePath, (err, stats) => {
+        if (err) {
+          console.error("Erro ao obter informações do arquivo: ", err);
+          return;
+        }
+
+        if (stats.isFile()) {
+          fs.unlink(imagePath, (err) => {
+            if (err) {
+              console.log(`Arquivo não excluido ${arquivo}`);
+              console.error("Erro ao excluir o arquivo: ", err);
+              return;
+            }
+          });
+        }
+      });
+    }
+  });
+});
---
+++ scripts/transfer-images-by-path.js
@@ -0,0 +1,49 @@
+// Este script transfer imagens com base nos caminhos contidos no arquivo txt
+
+const fs = require("fs");
+const { exec } = require("child_process");
+const path = require("path");
+
+// Caminho para o arquivo com a lista de imagens que deseja buscar
+const pathImageTxtFile = "caminho-do-arquivo.txt";
+
+// Diretório local onde as imagens serão salvas
+const dirLocalImages = "caminho-para-salvar-imagens";
+
+// Diretório onde as imagens se encontram
+const dirRemoteImages = "caminho-das-imagens";
+
+// DNS do servidor remote
+const remoteServer = "ecatalogos@d15.e-catalogos.net";
+
+const imagesToTransfer = fs
+  .readFileSync(pathImageTxtFile, "utf-8")
+  .split("\n")
+  .map((img) => img.trim())
+  .filter((img) => img.length > 0);
+
+const synchronizeImages = (imagem) => {
+  // Use esse trecho caso precise tranferir imagens de outro servidor
+  // const caminhoRemoto = `${remoteServer}:${path.join(
+  //   dirRemoteImages,
+  //   imagem
+  // )}`;
+
+  const caminhoRemoto = `${path.join(dirRemoteImages, imagem)}`;
+
+  const comando = `rsync -avz --progress ${caminhoRemoto} ${dirLocalImages}`;
+
+  exec(comando, (err, stdout, stderr) => {
+    if (err) {
+      console.error(`Erro ao sincronizar a imagem ${imagem}: `, err);
+      return;
+    }
+    if (stderr) {
+      console.error(stderr);
+    }
+  });
+};
+
+imagesToTransfer.forEach((imagem) => {
+  synchronizeImages(imagem);
+});
